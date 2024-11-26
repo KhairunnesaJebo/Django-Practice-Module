327c6e139e70947ce3355f16cb8855e48293c767
KhairunnesaJebo <jebo5902@gmail.com>
2024-11-26 23:01:23 +0600

    add

---
+++ Module 16 (Assignment_3)/copy_task_mgnt/task_model/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from task_category.models import Category
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ManyToManyField(Category)
+    is_completed = models.BooleanField(default=False)
+    assign_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
